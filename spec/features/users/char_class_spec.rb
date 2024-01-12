4fedc360329888319140f1689418671a51e2879b
Allan Evans <oneblankpage27@gmail.com>
2024-01-11 19:57:26 -0700

    Checked, but webmock still not working

--- spec/features/users/char_class_spec.rb
+++ spec/features/users/char_class_spec.rb
@@ -9,38 +9,93 @@ RSpec.describe 'Characters class page', type: :feature do
      name is entered and a class is selected, I am taken to a form that shows me saving throws assoeciated with that class " do
       first_response = File.read('spec/fixtures/first_char.json')
       wizard_response = File.read('spec/fixtures/wizard_class.json')
-      # VCR.use_cassette("create_char") do
-        visit '/'   
-        click_link("Log in with Discord")
-        
-        expect(page).to have_content("Welcome, bubba to Dungeons and Discord!!")
+      # stub_request(:get, "http://localhost:3000/api/v1/characters?user_id=#{@current_user.id}").
+      #    with(
+      #      headers: {
+      #     'Accept'=>'*/*',
+      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      #     'User-Agent'=>'Faraday v2.9.0'
+      #      }).
+      #    to_return(status: 200, body: first_response, headers: {})
 
-        expect(page).to have_content("New Character") 
-        
-        click_link("New Character")
-        
-        expect(page).to have_content("Name")
-        expect(page).to have_field(:name)
+      #    stub_request(:post, "http://localhost:3000/api/v1/characters").
+      #    with(
+      #      body: "{\"user_id\":#{@current_user.id},\"name\":\"Shrek\",\"char_class\":\"wizard\",\"race\":\"tiefling\",\"background\":\"Acolyte\",\"alignment\":\"lawful good\"}",
+      #      headers: {
+      #     'Accept'=>'*/*',
+      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      #     'Content-Type'=>'application/json',
+      #     'User-Agent'=>'Faraday v2.9.0'
+      #      }).
+      #    to_return(status: 200, body: "[]", headers: {})
 
-        expect(page).to have_content("Select Class")
-        expect(page).to have_field(:index)
+      #    stub_request(:get, "http://localhost:3000/api/v1/charclasses/wizard").
+      #    with(
+      #      headers: {
+      #     'Accept'=>'*/*',
+      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      #     'User-Agent'=>'Faraday v2.9.0'
+      #      }).
+      #    to_return(status: 200, body: wizard_response, headers: {})
 
-        fill_in :name, with: 'Shrek'
-        select('wizard', from: 'index')
-        select('tiefling', from: 'race')
-        select('lawful good', from: 'alignment')
-        click_button('Submit')
+      visit '/'   
+      click_link("Log in with Discord")
+      
+      expect(page).to have_content("Welcome, bubba to Dungeons and Discord!!")
+
+      expect(page).to have_content("New Character") 
+      
+      click_link("New Character")
+      
+      expect(page).to have_content("Name")
+      expect(page).to have_field(:name)
+
+      expect(page).to have_content("Select Class")
+      expect(page).to have_field(:index)
+
+      fill_in :name, with: 'Shrek'
+      select('wizard', from: 'index')
+      select('tiefling', from: 'race')
+      select('lawful good', from: 'alignment')
+      click_button('Submit')
 
 
-        expect(current_path).to eq("/users/prof_show")
+      expect(current_path).to eq("/users/prof_show")
 
-        expect(page).to have_content("Proficiency Choices")
-      # end
+      expect(page).to have_content("Proficiency Choices")
     end
 
     it "return to welcome view " do
       first_response = File.read('spec/fixtures/first_char.json')
       wizard_response = File.read('spec/fixtures/wizard_class.json')
+      # stub_request(:get, "http://localhost:3000/api/v1/characters?user_id=#{@current_user.id}").
+      #    with(
+      #      headers: {
+      #     'Accept'=>'*/*',
+      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      #     'User-Agent'=>'Faraday v2.9.0'
+      #      }).
+      #    to_return(status: 200, body: first_response, headers: {})
+
+      #    stub_request(:post, "http://localhost:3000/api/v1/characters").
+      #    with(
+      #      body: "{\"user_id\":#{@current_user.id},\"name\":\"Shrek\",\"char_class\":\"wizard\",\"race\":\"tiefling\",\"background\":\"Acolyte\",\"alignment\":\"lawful good\"}",
+      #      headers: {
+      #     'Accept'=>'*/*',
+      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      #     'Content-Type'=>'application/json',
+      #     'User-Agent'=>'Faraday v2.9.0'
+      #      }).
+      #    to_return(status: 200, body: "[]", headers: {})
+
+      #    stub_request(:get, "http://localhost:3000/api/v1/charclasses/wizard").
+      #    with(
+      #      headers: {
+      #     'Accept'=>'*/*',
+      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      #     'User-Agent'=>'Faraday v2.9.0'
+      #      }).
+      #    to_return(status: 200, body: wizard_response, headers: {})
 
       visit '/'   
       click_link("Log in with Discord")
